bfd66667f6a92820f35bcfdec58549d8fa502726
dzj <danzacjones@gmail.com>
2015-03-20 16:08:47 +1100

    Trim down templates.

--- config/org/capture-templates.el
+++ config/org/capture-templates.el
@@ -16,21 +16,13 @@
 
         ("w" "Words" entry (file+headline "~/org/learning/new_words.org" "New words"))
 
-	("cr" "crypto" entry (file+headline "~/org/learning/crypto.org" "Learning about crypto: papers, ideas of syllabus, threatmodelling"))
-
         ("u" "Unix" entry (file+headline "~/org/learning/unix_commands.org" "Unix Commands"))
 
         ("c" "C" entry (file+headline "~/org/learning/c_lang.org" "C Syntax"))
 
-        ("l" "Lua" entry (file+headline "~/org/learning/lua_syntax.org" "Lua Syntax"))
-
         ("g" "Git" entry (file+headline "~/org/learning/git_commands.org" "Git Commands"))
 
         ("n" "Networking" entry (file+headline "~/org/learning/networking.org" "Networking"))
-
-        ("p" "PHP" entry (file+headline "~/org/learning/PHP_syntax.org" "PHP Syntax"))
-
-	("br" "Breaking" entry (file+headline "~/org/learning/breaking.org" "Idea's for attacks, things to poke at, look into."))
 
 	("j" "Journal" entry (file+datetree "~/org/journal.org")
          "* %?\nEntered on %U\n  %i\n  %a")
