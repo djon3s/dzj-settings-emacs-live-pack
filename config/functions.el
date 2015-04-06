43012e6f2a6719c0352e0eba440bd89cbe3be4f7
dzj <danzacjones@gmail.com>
2015-04-07 02:47:04 +1000

    added function to copy open files so they can be saved to a text file

--- config/functions.el
+++ config/functions.el
@@ -13,3 +13,12 @@
 ;; I often forget the name of fill-column or fill-region
 ;; This is because I often search for "line wrap"
 (defun line-wrap 'fill-region)
+
+;; Useful for saving a list of open buffers if you don't want
+;; to have desktop save load all those other settings etc
+(defun copy-open-files ()
+  "Add paths to all open files to kill ring"
+  (interactive)
+  (kill-new (mapconcat 'identity
+                       (delq nil (mapcar 'buffer-file-name (buffer-list)))
+                       "\n")))
